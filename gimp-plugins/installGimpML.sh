810c4478429dcf5a7b4c987867658ec84ad0ab99
braytac <zat_xorg@hotmail.com>
2020-06-20 03:31:49 -0300

    - Corrección para arch: usar func. en lugar de alias - no commitear pyc's

--- gimp-plugins/installGimpML.sh
+++ gimp-plugins/installGimpML.sh
@@ -5,35 +5,44 @@ if [ ! -d "gimpenv" ]; then
 	if [ "$(uname)" == "Linux" ]; then
 		if [[ $(lsb_release -rs) == "18.04" ]]; then #for ubuntu 18.04
 			sudo apt install python-minimal
-			alias python='python2'
-		       
+			pyx () {
+			    python2 "$@"
+			}
+	       
 		elif [[ $(lsb_release -rs) == "20.04" ]]; then #for ubuntu 20.04
 			sudo apt install python2-minimal
 			wget https://bootstrap.pypa.io/get-pip.py 
-			alias python='python2'
-			python get-pip.py	
+			pyx () {
+			    python2 "$@"
+			}
+			pyx get-ipip.py	
 
 		elif [[ $(lsb_release -rs) == "10" ]]; then #for debian 10
 			wget https://bootstrap.pypa.io/get-pip.py 
-			python get-pip.py
+			pyx () {
+			    python "$@"
+			}
+			pyx get-pip.py
 		else
 			# archlinux
-			alias python='python2'
+			pyx () {
+			    python2 "$@"
+			}
 		fi
 	fi
 
-	python -m pip install --user virtualenv
-	python -m virtualenv gimpenv
+	pyx -m pip install --user virtualenv
+	pyx -m virtualenv gimpenv
 	source gimpenv/bin/activate
-	python -m pip install torchvision
-	python -m pip install opencv-python
-	python -m pip install numpy
-	python -m pip install future
-	python -m pip install torch
-	python -m pip install scipy
-	python -m pip install typing
-	python -m pip install enum
-	python -m pip install pretrainedmodels
+	pyx -m pip install torchvision
+	pyx -m pip install opencv-python
+	pyx -m pip install numpy
+	pyx -m pip install future
+	pyx -m pip install torch
+	pyx -m pip install scipy
+	pyx -m pip install typing
+	pyx -m pip install enum
+	pyx -m pip install pretrainedmodels
 	deactivate
 
 	echo "\n-----------Installed GIMP-ML------------\n"
